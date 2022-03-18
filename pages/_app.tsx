ba81736363e9cf4499e264f70d2ffd6ea66d9e27
Vincent Escoffier <56202201+reifocS@users.noreply.github.com>
2022-03-18 20:46:46 +0100

    seo

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,23 @@
+import Head from "next/head";
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <title>Sutom and tusmo solver</title>
+        <meta
+          name="twitter:title"
+          content="Sutom, tusmo solver"
+        />
+        <meta
+          property="og:description"
+          content="A solver for motus like games"
+        />
+      </Head>
+      <Component {...pageProps} />;
+    </>
+  );
 }
 
 export default MyApp;
